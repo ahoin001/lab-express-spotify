33cbfa902c0cbc33f62ab1c1fc3991124188c481
Alexander Hoinville <ahoin001@gmail.com>
2019-08-11 21:07:27 -0400

    Fix app.render to res.render , scold myself for silly mistake

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,43 +1,35 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-const SpotifyWebApi = require('spotify-web-api-node');
-
+var SpotifyWebApi = require('spotify-web-api-node');
 
 // set view engine and tell where to look for views
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-
-
 // setting the spotify-api goes here:
 const clientId = 'b0741ec345814020b165aec49cc3d82d',
     clientSecret = 'a93f7da8810f48e6bd7e763369fe5e71';
 
-// Use permissions to make requests to spotify api
-const spotifyApi = new SpotifyWebApi({
+var spotifyApi = new SpotifyWebApi({
     clientId: clientId,
     clientSecret: clientSecret
 });
 
-// Retrieve an access token
+// Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
-    .then(data => {
+    .then(function (data) {
         spotifyApi.setAccessToken(data.body['access_token']);
-    })
-    .catch(error => {
-        console.log('Something went wrong when retrieving an access token', error);
-    })
-
+    }, function (err) {
+        console.log('Something went wrong when retrieving an access token', err);
+    });
 
 // the routes go here:
 app.get('/', function (req, res) {
     // console.log(req);
-    app.render("home.hbs");
-  })
+    res.render("home");
+})
 
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
