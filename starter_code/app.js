345a5eccde45db191b938bdee6eec076847cd268
Alexander Hoinville <ahoin001@gmail.com>
2019-08-12 14:51:50 -0400

    Add documentation , search artists and see their albums features

--- starter_code/app.js
+++ starter_code/app.js
@@ -32,8 +32,38 @@ app.get('/', function (req, res) {
 })
 
 app.get('/artists', function (req, res) {
-    // console.log(req);
-    res.render("artist");
+
+    // When form from homepage submitted, its info will be passed here
+    // console.log("req ======= ", req.query);
+
+    // Search artists whose name contains any search term
+    spotifyApi
+        //    theArtistName is from name attribute in our search form
+        .searchArtists(req.query.theArtistName)
+        .then(data => {
+            // console.log('Search artists by: ====', data.body.artists.items);
+            // allTheArtists is an array which we give the list of all found artists in spotify api
+            res.render('artists', { allTheArtists: data.body.artists.items });
+        })
+        .catch(err => console.log("Error while getting the artists: ", err));
+
+})
+
+// artistId is a router parameter , its value is provided when the user makes request to /albums/(whateverartistidhere)
+app.get('/albums/:artistId', (req, res, next) => {
+
+    // req.params is object with key - value pair of router parameter name, and whatever value is requested by user
+    // console.log("Id is: ", req.params);
+    spotifyApi
+        .getArtistAlbums(req.params.artistId, { limit: 5 })         // spotify api uses artistId to retrieve albums from the artist
+        .then(data => {
+            
+             //console.log(" = = == = == = = = =",data.body);
+             // passes array (.items) of albums from artist
+            res.render('albums', { allTheAlbums: data.body.items });
+        })
+        .catch(err => console.log("Error while getting the albums: ", err));
+
 })
 
 
